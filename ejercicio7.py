a90fb02d56c8e0d5703eec095087917848168261
Joel Barrionuevo <82744729+joelb-2011@users.noreply.github.com>
2021-12-26 23:13:26 -0500

    Delete ejercicio7.py

--- ejercicio7.py
+++
@@ -1,8 +0,0 @@
-#Ejercicio 7: Dado un (1) número, imprimir 0 si es par y 1 si es impar.
-
-numero = int(input("Ingrese un número: "))
-
-if numero % 2 == 0:
-    print("El número es par.")
-else:
-    print("El número es impar.")
