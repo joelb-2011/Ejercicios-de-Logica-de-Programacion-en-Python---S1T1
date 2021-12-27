67fb909b064a49df7d5a1266b4033c2316af9c6c
Joel Barrionuevo <82744729+joelb-2011@users.noreply.github.com>
2021-12-26 23:12:59 -0500

    Delete ejercicio22.py

--- ejercicio22.py
+++
@@ -1,6 +0,0 @@
-#Ejercicio 22: Dado un número entero N, calcular e informar al usuario cuántos dígitos tiene dicho número.
-
-numero = int(input("Ingrese cualquier número: "))
-contador = len(str(numero))
-
-print("Usted ha ingresado", contador,"dígitos.")
