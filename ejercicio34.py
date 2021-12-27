2b05f0ab8ae52c6172ad3c5981516ae31941b12c
Joel Barrionuevo <82744729+joelb-2011@users.noreply.github.com>
2021-12-26 23:13:10 -0500

    Delete ejercicio34.py

--- ejercicio34.py
+++
@@ -1,5 +0,0 @@
-#Ejercicio 34: Escriba una función que calcule el perímetro del pentágono (siendo el perímetro la suma de los lados del polígono).
-
-lado = int(input("Ingrese uno de los lados del pentágono en cm: "))
-perimetro = lado * 5
-print("El perímetro del pentágono es de:", perimetro, "cm.")
