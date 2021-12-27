b9c9cfcfb52045624536db6f873bf729b76653af
Joel Barrionuevo <82744729+joelb-2011@users.noreply.github.com>
2021-12-26 23:13:05 -0500

    Delete ejercicio29.py

--- ejercicio29.py
+++
@@ -1,8 +0,0 @@
-#Ejercicio 29: Construye un algoritmo que calcule e imprima la tabla de multiplicar, desde la tabla del 1 hasta la del 10.
-
-inicio = 1
-fin = 10
-
-for factor in range(inicio, fin + 1):  
-    for f in range(inicio, fin + 1):
-	    print(f"{factor} x {f} =  {factor * f}")
